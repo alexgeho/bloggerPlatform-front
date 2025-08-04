bd0fa0d67121ea296e1bc87d8665fbe99881cbf1
Alexander Gerhard <870717ag@gmail.com>
2025-08-04 11:46:59 +0200

    fix: main page appearance

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
   plugins: [react()],
-  base: "/bloggerPlatform-front/",
-
+  base: isProduction ? '/bloggerPlatform-front/' : '/',
 })
